44c82af53ce56ba26119a09a91cd001b6485d25c
Alberto <albertoglezb@hotmail.com>
2024-01-26 13:52:23 -0600

    The CORS configuration was added.

--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -7,6 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => 
+{ 
+    options.AddPolicy("AllowAll", 
+        b => b.AllowAnyHeader().
+        AllowAnyOrigin().
+        AllowAnyMethod()); 
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
