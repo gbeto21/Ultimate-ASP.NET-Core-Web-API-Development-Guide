ccf91ace57a03536832a064591433dde5f0ac627
Alberto <albertoglezb@hotmail.com>
2024-02-02 07:37:25 -0600

    The migration to seed the database was created.

--- HotelListingAPI/Data/HotelListingDbContext.cs
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -2,14 +2,51 @@
 
 namespace HotelListingAPI.Data
 {
-    public class HotelListingDbContext:DbContext
+    public class HotelListingDbContext : DbContext
     {
-        public HotelListingDbContext(DbContextOptions options) : base(options) {}
-        
-        /*Will turn those two into tables: Hotels and Countries tables*/
-        public DbSet<Hotel> Hotels {  get; set; }
+        public HotelListingDbContext(DbContextOptions options) : base(options) { }
 
-        public DbSet<Country> Countries {  get; set; }
+        /*Will turn those two into tables: Hotels and Countries tables*/
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        /*Method to seed the DB*/
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country { Id = 1, Name = "Jamaica", ShortName = "JM" },
+                new Country { Id = 2, Name = "Bahamas", ShortName = "BS" },
+                new Country { Id = 3, Name = "Cayman Island", ShortName = "CI" }
+            );
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 4
+                }
+            );
+        }
 
     }
 }
--- HotelListingAPI/Migrations/20240202133059_SeededCountriesAndHotels.Designer.cs
+++ HotelListingAPI/Migrations/20240202133059_SeededCountriesAndHotels.Designer.cs
@@ -3,6 +3,7 @@ using HotelListingAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelListingAPI.Migrations
 {
     [DbContext(typeof(HotelListingDbContext))]
-    partial class HotelListingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240202133059_SeededCountriesAndHotels")]
+    partial class SeededCountriesAndHotels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
