f25799bb5070d62eb0515557697599f75cc02f2d
Alberto <albertoglezb@hotmail.com>
2024-02-02 11:20:31 -0600

    The request model class: CreateContry was added.

---
+++ HotelListingAPI/Models/Country/CreateContryDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListingAPI.Models.Country
+{
+    public class CreateContryDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+    }
+}
