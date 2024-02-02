1a1d3253d88586b493eb9afdfd4fe389ad63c720
Alberto <albertoglezb@hotmail.com>
2024-02-02 07:09:12 -0600

    The creation of the Country and Hotel tables was implemented.

---
+++ HotelListingAPI/Data/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelListingAPI.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public virtual IList<Hotel> Hotels { get; set; }
+    }
+}
--- HotelListingAPI/Data/HotelListingDbContext.cs
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -5,5 +5,11 @@ namespace HotelListingAPI.Data
     public class HotelListingDbContext:DbContext
     {
         public HotelListingDbContext(DbContextOptions options) : base(options) {}
+        
+        /*Will turn those two into tables: Hotels and Countries tables*/
+        public DbSet<Hotel> Hotels {  get; set; }
+
+        public DbSet<Country> Countries {  get; set; }
+
     }
 }
