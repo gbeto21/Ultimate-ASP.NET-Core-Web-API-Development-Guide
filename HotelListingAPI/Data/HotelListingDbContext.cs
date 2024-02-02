5a5003b75a9ecd6ccb20155bad57c0bffe7fb471
Alberto <albertoglezb@hotmail.com>
2024-02-01 19:36:19 -0600

    Initial DBConfiguration was sett up.

---
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Data
+{
+    public class HotelListingDbContext:DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options) {}
+    }
+}
